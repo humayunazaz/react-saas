f722163fafc69f4a77e5eeb17d68ad89c1dc9bb1
humayunazaz <humayun.azaz@gmail.com>
2024-04-09 02:38:35 +0500

    the stepper style partially update

--- src/pages/Questionnaire.tsx
+++ src/pages/Questionnaire.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import QuestionnaireStepper from '../components/questionnaire/questionnaire-stepper';
 
 const QuestionnairePage = () => {
-  return <div>QuestionnairePage</div>;
+  return (
+    <div className='p-5'>
+      <div className='questionnaireStepperContainer'>
+        <QuestionnaireStepper />
+      </div>
+    </div>
+  );
 };
 
 export default QuestionnairePage;
